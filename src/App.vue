035c50d8853ea74784245f756629e63ad8c7a3e1
fuzhaoyang <932647051@qq.com>
2024-02-28 14:08:08 +0800

    feat:雪碧图生成工具框架搭建

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="app">
+         <h2>上传图片</h2>
+        <form action="/upload" method="POST" enctype="multipart/form-data">
+            <input type="file" name="image">
+            <button type="submit">上传</button>
+        </form>
+        <div class="icon-APP_manager"></div>
+    </div>
+</template>
+<style scoped>
+    @import './assets/sprite.css';
+</style>
+<script>
+    export default {
+        data() {
+            return {}
+        },
+        components: {}
+    }
+</script>
