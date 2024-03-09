5ad6834712603db261e7b8b8b8447df77fcee957
fuzhaoyang <932647051@qq.com>
2024-03-09 12:23:25 +0800

    feat:图片类型转换工具开发

--- server.js
+++ server.js
@@ -235,7 +235,7 @@ app.post('/upload/convert', (req, res) => {
           const imgData = fs.readFileSync(list.path)
           base64Data.push({
             name: list.name,
-            url: 'data:image/png;base64,' + Buffer.from(imgData).toString('base64'),
+            url: `data:image/${type.toLowerCase()};base64,` + Buffer.from(imgData).toString('base64'),
             id: uuidv4()
           })
         } catch (error) {
@@ -250,6 +250,99 @@ app.post('/upload/convert', (req, res) => {
   })
 })
 
+// 处理图片上传
+app.post('/upload/compress', (req, res) => {
+  const { uuid,config } = req.headers
+  const {compress,quality,rotate,scale,greyscale,resize} = JSON.parse(config);
+  if (fs.existsSync(`./compress/original/${uuid}`)) {
+    if (fs.readdirSync(`./compress/original/${uuid}`).length) {
+      deleteFolderRecursive(`./compress/original/${uuid}`)
+    }
+  } else {
+    fs.mkdir(`./compress/original/${uuid}`, (err) => {
+      if (err) {
+        console.log('文件夹创建失败!')
+      } else {
+        console.log('文件夹创建成功!')
+      }
+    })
+  }
+  if (fs.existsSync(`./compress/target/${uuid}`)) {
+    if (fs.readdirSync(`./compress/target/${uuid}`).length) {
+      deleteFolderRecursive(`./compress/target/${uuid}`)
+    }
+  } else {
+    fs.mkdir(`./compress/target/${uuid}`, (err) => {
+      if (err) {
+        console.log('文件夹创建失败!')
+      } else {
+        console.log('文件夹创建成功!')
+      }
+    })
+  }
+  // 设置存储引擎和文件名
+  const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, `./compress/original/${uuid}`)
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname) // 设置文件名
+    }
+  })
+  const upload = multer({ storage: storage })
+  const imgList = []
+  const base64Data = []
+  upload.array('image')(req, res, (err) => {
+    req.files.forEach(item => {
+      Jimp.read(item.path).then(img => {
+        let image = img;
+        if (quality.key) {
+          console.log(1)
+          image = image.quality(quality.value)
+        }
+        rotate,scale,greyscale,resize
+        if (rotate.key) {
+          console.log(2)
+          image = image.rotate(rotate.value)
+        }
+        if (scale.key) {
+          console.log(3)
+          image = image.rotate(scale.value)
+        }
+        if (greyscale.key) {
+          console.log(4)
+          image = image.rotate(greyscale.value)
+        }
+        if (resize.key) {
+          image = image.resize(resize.width, resize.height)
+        }
+      return image.write(`./compress/target/${uuid}/${item.filename}`);
+    })
+      imgList.push({
+        path: `./compress/target/${uuid}/${item.filename}`,
+        name: `${item.filename}`
+      })
+    })
+    setTimeout(() => {
+      imgList.forEach((list) => {
+        try {
+          const imgData = fs.readFileSync(list.path)
+          base64Data.push({
+            name: list.name,
+            url: `data:image/${list.name.split('.')[1]};base64,` + Buffer.from(imgData).toString('base64'),
+            id: uuidv4()
+          })
+        } catch (error) {
+          console.log(err)
+        }
+      })
+      res.send({
+        status: 200,
+        data: base64Data
+      })
+    }, 2000)
+  })
+})
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
